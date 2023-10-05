332ffb48229dd350efdf4fd5cdee1eb072b7925e
Hassen <hassen.saidi@gmail.com>
2023-10-04 17:09:21 -0700

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,3 +1,6 @@
+// SQLite requires zlib and readline
+// This build makes sure that the linker finds locally installed zlib and readline
+
 fn main() {
     println!("cargo:rustc-flags=-lz -lreadline");
 }
